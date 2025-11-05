6a8c496303cab5350404dbc9f1fd4e5e03fe6888
easyhooon <mraz3068@gmail.com>
2025-11-05 09:26:54 +0900

    [chore] target, compile sdk version upgrade (35 -> 36)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.github.easyhooon.hangul_converter"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.github.easyhooon.hangul_converter"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
