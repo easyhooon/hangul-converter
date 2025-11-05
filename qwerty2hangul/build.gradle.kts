e77e21dab8c3e832e9be302e9cf0c18cacd76e07
easyhooon <mraz3068@gmail.com>
2025-11-05 13:08:09 +0900

    feat: 라이브러리 모듈 배포 환경 구축

--- qwerty2hangul/build.gradle.kts
+++ qwerty2hangul/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("java-library")
     kotlin("jvm")
+    alias(libs.plugins.vanniktech.maven.publish)
 }
 
 java {
@@ -14,4 +17,43 @@ kotlin {
 
 dependencies {
     testImplementation(libs.junit)
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.easyhooon",
+        artifactId = "qwerty2hangul",
+        version = "1.0.0"
+    )
+
+    pom {
+        name.set("Qwerty2Hangul")
+        description.set("Convert QWERTY-typed Korean to actual Hangul characters")
+        inceptionYear.set("2025")
+        url.set("https://github.com/easyhooon/qwerty2hangul")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("easyhooon")
+                name.set("Lee jihun")
+                email.set("mraz3068@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/easyhooon/qwerty2hangul")
+            connection.set("scm:git:git://github.com/easyhooon/qwerty2hangul.git")
+            developerConnection.set("scm:git:ssh://git@github.com/easyhooon/qwerty2hangul.git")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
